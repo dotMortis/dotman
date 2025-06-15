d8f3beb43b0fef1b5d19be021031dcdc87d79416
dotM <29275165+dotMortis@users.noreply.github.com>
2025-06-15 23:19:11 +0200

    update docker

--- bin/arch.sh
+++ bin/arch.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 if ! docker ps -a --format '{{.Names}}' | grep -q "arch-container"; then
-  docker run -it --name arch-container -v /home/nkoel/projects/dotman:/workspace archlinux /bin/bash -c "bash /workspace/docker/prepare.sh && /bin/bash"
+  docker run -it \
+    --name arch-container \
+    --privileged \
+    -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+    -v /home/nkoel/projects/dotman:/workspace \
+    archlinux \
+    /bin/bash -c "bash /workspace/bin/prepare.sh && /bin/bash"
 else
   docker start -i arch-container
 fi
